cb59ad0efb4ce6e7963e9ae6bcf9778e3bf82ec0
Arooba Nadeem <nadeemarooba0@gmai.com>
2023-03-25 21:25:42 +0500

    completed home and contact us page

---
+++ playground/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from playground.models import Contact
+from datetime import datetime
+# Create your views here.
+# Request---> Response
+#Request handler
+#in some fameworks it is called action
+def home(request):
+    #pull data from db
+    #transform
+    #send Email
+  #  return HttpResponse({"THIS IS HOME PAGE"})
+      return render(request,'index.html')
+
+def about(request):
+            return render(request,'about.html')
+
+    #return HttpResponse({"THIS IS ABOUT PAGE"})
+def categories(request):
+              return render(request,'categories.html')
+
+    #return HttpResponse({"THIS IS CATEGORIES PAGE"})
+def contact(request):
+              
+             if request.method =='POST':
+                     name=request.POST.get('name')
+                     email=request.POST.get('email')
+                     phone=request.POST.get('phone')
+                     feedback=request.POST.get('feedback')
+                     city=request.POST.get('city')
+                     contact=Contact(name=name , email=email, phone=phone ,feedback=feedback, city=city, date=datetime.today())
+                     contact.save()
+             return render(request,'contact.html')
+
+    #return HttpResponse({"THIS IS CONTACT PAGE"})
